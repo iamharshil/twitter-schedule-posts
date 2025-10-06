0b6a409bdae3e4fbd586426d86df1c46861ac9f1
Harsh <harshilc71@gmail.com>
2025-10-06 23:17:53 +0530

    Logout button loader

--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -2,28 +2,45 @@
 
 import { LogOut } from "lucide-react"
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button"
 
 export function LogoutButton() {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
+
     const onLogout = async () => {
-        await fetch("/api/auth/logout")
-            .then((res) => res.json())
-            .then((res) => {
-                if (res?.success) {
-                    toast.success("Logged out successfully");
-                    router.push("/auth");
-                } else {
-                    toast.error(res?.message || "Failed to logout");
-                }
-            })
+        setLoading(true);
+        try {
+            const res = await fetch("/api/auth/logout");
+            const data = await res.json();
+
+            if (data?.success) {
+                toast.success("Logged out successfully");
+                router.push("/auth");
+            } else {
+                toast.error(data?.message || "Failed to logout");
+            }
+        } catch (_) {
+            toast.error("Failed to logout");
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
-        <Button variant="secondary" onClick={onLogout}>
-            <LogOut className="mr-2 size-4" />
-            Logout
+        <Button variant="secondary" onClick={onLogout} disabled={loading}>
+            {loading ? (
+                <span className="flex items-center mx-6">
+                    <span className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-current border-t-transparent" aria-hidden="true"></span>
+                </span>
+            ) : (
+                <>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    Logout
+                </>
+            )}
         </Button>
     )
 }
