51921266471928b216223e4a8fbfff08aea4da71
Harsh <harshilc71@gmail.com>
2025-10-12 13:00:18 +0530

    Schdule api using cron

---
+++ src/app/api/scheduler/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server"
+var cron = require("node-cron");
+
+export async function GET(req: Request) {
+    try {
+        cron.schedule("*/15 * * * *", async () => {
+            console.log("Running cron...")
+            await fetch(`${process.env.NEXT_PUBLIC_SITE_URL as string}/api/cron-webhook`, {
+                headers: new Headers({
+                    "Authorization": process.env.API_KEY as string
+                })
+            });
+        })
+
+        return NextResponse.json({ message: "Cron scheduled successfully" }, { status: 200 })
+    } catch (error) {
+        console.error("error:", error)
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+    }
+}
